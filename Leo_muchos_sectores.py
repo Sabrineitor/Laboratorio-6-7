d113ed1240f446409a83cca2ad8d4cd03a019fcf
Sabrina Benas <48727724+Sabrineitor@users.noreply.github.com>
2019-10-30 00:27:34 -0300

    actualizo (29/10)

--- Leo_muchos_sectores.py
+++ Leo_muchos_sectores.py
@@ -10,48 +10,34 @@ import sys
 
 frec_adq = 23000
 
-#tiempo = np.arange(0, len(datos)*1/frec_adq, 1/frec_adq)
-#tiempo = np.arange(0, len(datos)*dt_adq, dt_adq)
-
 programa = 'read'               # Programa compilado a partir de readSd.c
 
-ttot = 120 #en segundos
+ttot = 400 #en segundos
 tmed  = 3000.0 * 256 /frec_adq # Duracion de cada medicion: cantidad de sectores medidos por el tamaño del sector
 								#tiene que coincidir el numero de sectores en ambos programas!!!
 
-print(tmed)
-cantidad = int(ttot/tmed)       # Cantidad de mediciones.
-print(cantidad)
+#print(tmed) con 3000 sect es 34 seg aprox
+
+cantidad = int(ttot/tmed)       # Cantidad de iteraciones
+print('cant de iteraciones ' + str(cantidad))
 
 Datos = [] #es vector de vectores
 
 for j in range(cantidad):
     # Ejecuta el programa de lectura de 1 medicion (ie 400 sectores). La salida
-    # del programa es un file dec.dat con los valores 
-    call(["./" + programa, str(int(j))]) #Agregar arg al programa? 
-    #sin el str j tengo 3 audios iguales
-    # que es el j????
-    datos1 = np.loadtxt('dec.dat') #CONSULTAR SI PODEMOS TENER 3 DEC
-    #Datos.append(datos1)
-    sonido_normalizado = datos1/np.max(np.abs(datos1))
-    sonido_normalizado = sonido_normalizado * 32767 #para tener int16 deforma el volumen 
-    sonido_normalizado = sonido_normalizado.astype('int16')
-    wav.write('audio_' + format(j) + '.wav',int(frec_adq),sonido_normalizado)
-
-
-
-sys.exit(0)
-
-
-
+    # del programa es un file dec.dat con los valores de los dos canales
+    call(["./" + programa, str(int(j))]) #Agregar arg al programa?
+    dat_file = glob.glob('dec.dat')[0]
+    datos1 = np.loadtxt('dec.dat')
+    Datos.append(datos1)
+    
 
 datos = []
 for i in range(len(Datos)):
-	for j in range(len(Datos[i])):
-		datos.append(Datos[i][j])
+    for j in range(len(Datos[i])):
+        datos.append(Datos[i][j])
 
-print(len(datos))
-#print(datos)
+print('cant de datos leidos ' + str(len(datos)))
 
 sonido_normalizado = datos/np.max(np.abs(datos))
 sonido_normalizado = sonido_normalizado * 32767 #para tener int16 deforma el volumen 
@@ -59,4 +45,23 @@ sonido_normalizado = sonido_normalizado.astype('int16')
 wav.write('audio.wav',int(frec_adq),sonido_normalizado)
 
 
+
+
+##### Paso datos a txt para analisis (esto mucho no sirve porque no lo puedo levantar con np.loadtxt)
+#np.savetxt('todos_los_datos.txt', datos, delimiter = '\t')
+
+
+
+##### Grafico
+"""
+tiempo = np.arange(0, len(datos)*1/frec_adq, 1/frec_adq)
+
+plt.plot( tiempo,datos, 'o-')
+plt.tick_params(labelsize=13)
+plt.legend()
+plt.xlabel('Tiempo [s]', size = 13)
+#ax1.set_ylabel('Intensidad [s.u.]')
+plt.ylabel('Amplitud', size = 13)
+plt.grid(True)
 plt.show()
+"""
